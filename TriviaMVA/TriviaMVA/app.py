b8b945bc1eed84608d9df70fb026b22eb15c8987
JoeyDing <dydydy2012@163.com>
2017-11-30 22:30:41 +0800

    Injected html as returned content

--- TriviaMVA/TriviaMVA/app.py
+++ TriviaMVA/TriviaMVA/app.py
@@ -2,19 +2,29 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
-
+#Do Not Touch
 from flask import Flask
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
+#End Do Not Touch
 
-
+# server/
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return "Hello World!"
+    #return "Hello World!"
+    return """<html>
+                       <head>
+                               <title>Hello, world</title>
+                       </head>
+                       <body>
+                                <h1>Hello,Joey</h1>
+                       </body>
+                   </html>"""
 
+#Launching our server
 if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
