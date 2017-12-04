7532b759cad1b77f6d3558b0f278100dcc66e352
dydydy2012@163.com <dydydy2012@163.com>
2017-12-04 17:11:52 +0800

    finish loop module

--- TriviaMVA/LoopsModule/LoopsModule.py
+++ TriviaMVA/LoopsModule/LoopsModule.py
@@ -29,6 +29,7 @@ turtle.color('red')
 #turtle.right(500)
 """
 
+"""
 # draw a "tian" in chiense
 turtle.color('green')
 for step in range(4):
@@ -70,3 +71,47 @@ turtle.color('purple')
 for step in range(4):
     turtle.forward(100)
     turtle.right(360/3)
+
+"""
+
+"""
+#draw custom shape
+nbrsides = 20
+turtle.color('green')
+for steps in range(nbrsides):
+    turtle.forward(50)
+    turtle.right(360/nbrsides)
+    for moresteps in range(nbrsides):
+        turtle.forward(30)
+        turtle.right(360/nbrsides)
+"""
+
+#print 0,1,2
+for steps in range(3):
+    print(steps)
+
+#print 1,2,3 , never reach the higher number
+for steps in range(1,4):
+    print(steps)
+
+#print 1,3,5,7,9 ,third parameter skip by 
+for steps in range(1,10,2):
+    print(steps)
+
+# foreach loop
+for steps in [1,2,3]:
+    print(steps)
+
+# foreach loop
+for color in ['red','black','green','blue']:
+    turtle.color(color)
+    turtle.forward(100)
+    turtle.right(90)
+
+# while loop : do sth while a condition is true.
+#this is where while loop shines, as long as they keep getting this answer we will stayhere until they get this right
+answer = '0'
+
+while answer != '4':
+    answer = input('What is 2+2 ? ')
+print('Yes! 2+2 = 4')
