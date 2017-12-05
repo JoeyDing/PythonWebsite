200594abe1282756779d96939f5b2735cb52ca84
dydydy2012@163.com <dydydy2012@163.com>
2017-12-05 17:48:41 +0800

    Finish the practice of functions module

---
+++ TriviaMVA/FuncitonsModule/FuncitonsModule.py
@@ -0,0 +1,22 @@
+def main():
+    #printMessge('hello')
+    printMessge(getMessage('DDD'))
+    return
+
+
+def getMessage(name):
+    message = 'Hello, ' + name
+    return message
+    #return
+
+
+def printMessge(message):
+    names =['a','b','c','d']
+    for name in names:
+        print(name +' '+ message)
+    #print('Hellow,world!')
+    return
+
+main()
+#output = getMessage('Joey')
+#printMessge(output)
