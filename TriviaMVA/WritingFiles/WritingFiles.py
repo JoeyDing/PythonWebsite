d6454e92ad9a3587729c4e30364fe24428c52576
dydydy2012@163.com <dydydy2012@163.com>
2017-12-05 17:11:34 +0800

    Finish the practice of file reading and writing

--- TriviaMVA/WritingFiles/WritingFiles.py
+++ TriviaMVA/WritingFiles/WritingFiles.py
@@ -13,12 +13,12 @@ file = open(filename,accessmode)
 #file = open(filename,mode = WRITE)
 file.write('\n'+data)
 #file.write('How are you,line2?')
-#open sth, close it, like open a doc inside word, with the os says you are using it, you program might crashes cos yout file is used by someone else.
+#open sth, close it, like open a doc inside word, with the os says you are using it, you program might crashes cos yout file is in use.
 file.close()
 
 print('File written successfully')  
 """
-
+"""
 #Reading file
 employeefileName = 'employees.txt'
 accessMode = 'r'
@@ -34,6 +34,23 @@ for employee in range(0,num_lines):
     fileContent = employeeFile.readline()
     employeeList.append(fileContent)
 print(employeeList)
+
+employeeFile.close()
+"""
+
+import csv
+accessmode = 'r'
+employeefileName = 'employees.txt'
+with open(employeefileName,accessmode) as employeesFile:
+    allRowsList = csv.reader(employeesFile)
+    for currentRow in allRowsList:
+        #a little bit weird sytax, cos ',' below is not seperator, it's sth to de displeyed between two words.
+        print(','.join(currentRow))
+          #for currentWord in currentRow:
+          #      print(currentWord)
+
+
+
 
 
 
